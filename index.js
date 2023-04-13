57851aa7b9125934e17696d435595445e9b264a2
RJaime-jr <Ricoa.jaime@outlook.com>
2023-04-12 17:21:54 -0700

    made the pixels scale down if we need a hint, or if answer submitted it false.

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 let c = document.createElement("canvas");
 let img1 = new Image();
+let helpBut = document.getElementById("hint");
+let pixelNum = [1, 4, 8, 16, 32, 64];
+let arrNum = 5;
+let imgBox = document.getElementById("imgBox");
+let level = document.getElementById("level");
+let enter = document.getElementById("enter");
+let answer = false;
 
 
 img1.onload = function () {
     document.getElementById('image1').remove();
+    loadImage();
+
+}
+
+
+function loadImage() {
+    //document.getElementById('image1').remove();
     w = img1.width;
     h = img1.height;
 
@@ -15,7 +29,7 @@ img1.onload = function () {
 
 
     let pixelArr = ctx.getImageData(0, 0, w, h).data;
-    let sample_size = 4;
+    let sample_size = pixelNum[arrNum];
     for (let y = 0; y < h; y += sample_size)
         for (let x = 0; x < w; x += sample_size) {
             let p = (x + (y * w)) * 4;
@@ -26,8 +40,44 @@ img1.onload = function () {
     let img2 = new Image();
     img2.src = c.toDataURL("image/jpeg");
     img2.width = 600;
-    document.body.appendChild(img2);
+    img2.id = ("img2")
+    imgBox.appendChild(img2);
+    //level.textContent = "Hint: " + arrNum;
+    helpBut.textContent = "Hint: " + arrNum;
+}
+
+function reloadImage() {
+    arrNum--;
+    if (arrNum < 0) {
+        arrNum = 5;
+    }
+    document.getElementById('img2').remove();
+    loadImage();
+    console.log(arrNum);
+
+
 }
 
 img1.src = document.getElementById("image1").src;
 
+
+
+helpBut.onclick = function () {
+    reloadImage();
+}
+
+
+enter.onclick = function () {
+    // console.log(false);
+    if (answer == false) {
+        reloadImage();
+        console.log(answer);
+
+
+
+    }
+    else {
+        console.log(answer);
+
+    }
+}
